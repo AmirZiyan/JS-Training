dc0f61de09a99bf3aed11fbbd5bbeafbd8597a1c
Amir Ziyan <ziyan0770@gmail.com>
2025-06-10 14:43:52 +0530

    Standard Date and time + Some Built-in FUnctions in JS

---
+++ MODULE/mod-2.js
@@ -0,0 +1,25 @@
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+console.log(array);
+
+array.push(9);
+
+console.log(array);
+
+array.unshift(0);
+
+console.log(array);
+
+console.log(array.length);
+
+//Function is a block of code that does a specific task.
+
+function doesAnything(name) {
+  console.log("Hello, World! " + name);
+}
+
+doesAnything("Gosens");
+doesAnything("Grimaldo");
+doesAnything("Cherki");
+doesAnything("Alberto");
+doesAnything("Miralles");
