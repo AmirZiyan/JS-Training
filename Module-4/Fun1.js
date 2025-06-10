9a08329419b681f4dd8f556a17066b2afb42eef4
Amir Ziyan <ziyan0770@gmail.com>
2025-06-10 14:48:38 +0530

    Adding Function One File

--- Module-4/Fun1.js
+++ Module-4/Fun1.js
@@ -39,3 +39,39 @@ function multiplication(c, d) {
 multiplication(3, 5);
 multiplication(5, 8);
 multiplication(1, 4.3);
+
+//Division
+
+console.log("Division");
+function Division(e, f) {
+  div = 0;
+  div = e / f;
+  console.log(div);
+}
+
+Division(4, 2);
+Division(100, 3);
+Division(30, 3);
+
+const greet = function (name) {
+  console.log(`Hello ` + name + " !");
+};
+
+greet("Emily");
+greet("Nui");
+
+//Even Function
+console.log("Even Function");
+
+function Even(num) {
+  if (num % 2 == 0) {
+    console.log(num + " is even");
+  } else {
+    console.log(num + " is odd");
+  }
+}
+
+Even(3);
+Even(8);
+Even(65);
+
---
+++ Module-4/FunExp.js
@@ -0,0 +1,10 @@
+var add = function (number1, number2) {
+  return number1 + number2;
+};
+
+let a = add(2, 4);
+console.log(a);
+let b = a;
+console.log(b);
+let c = add(5, 7);
+console.log(c);
